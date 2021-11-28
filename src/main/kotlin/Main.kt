548edb33312838aeb3eb2a97a3d7c233aaed51e9
Kamil Bąk <kamilbak113@gmail.com>
2021-11-28 10:28:01 +0100

    Fixed layout for bigger numbers; Fixed uiScope not being destroyed while closing app

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -58,10 +58,11 @@ fun main(vararg args: String) = application {
 	Window(
 		onCloseRequest = {
 			workerScope.cancel()
+			uiScope.cancel()
 			exitApplication()
 		},
 		resizable = false,
-		state = rememberWindowState(size = DpSize(320.dp, 224.dp)),
+		state = rememberWindowState(size = DpSize(320.dp, 256.dp)),
 		icon = icon,
 		title = "Monte Carlo PI Calculator"
 	) {
